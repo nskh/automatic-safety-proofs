7dd7544d5686e93cde8b57662bf36662417f15c3
elanor <elanor@umich.edu>
2021-06-16 17:19:05 -0400

    testing safety grid and active corner

---
+++ active_corners_grid.py
@@ -0,0 +1,27 @@
+from utils import *
+if __name__ == '__main__':
+    r = 1
+    hexagon = RegularPolygon(Point(0, 0), r, 6)
+    # plot_polygon(hexagon)
+    diamond = RegularPolygon(Point(0, 0), r, 4)
+    # plot_polygon(diamond)
+    w = 0.5
+    square_points: list = [geometry.Point(val) \
+                           for val in [[w, -w], [w, w], [-w, w], [-w, -w]]]
+    square: geometry.Polygon = Polygon(*square_points)
+    # plot_polygon(square)
+    rect_points: list = [geometry.Point(val) \
+                         for val in [[2 * w, -w], [2 * w, w], [-2 * w, w], [-2 * w, -w]]]
+    rectangle: geometry.Polygon = Polygon(*rect_points)
+    # plot_polygon(rectangle)
+
+    x, y = symbols("x y")
+    traj_r = 4
+    circle_traj = x ** 2 + y ** 2 - traj_r ** 2  # radius 4
+    sin_traj = sin(x) - y
+
+    # plot_safe_grid(hexagon, circle_traj, (-5, 5), (-5, 5))
+    # plot_safe_grid(rectangle, sin_traj, (-6, 6), (-3, 3))
+    print(outside_active_corners(diamond, circle_traj, Point(0,4), Point(0,6), x, y), True)
+    print(outside_active_corners(diamond, circle_traj, Point(0,4), Point(0,4), x, y), False)
+    print(outside_active_corners(diamond, circle_traj, Point(0,4), Point(0,5), x, y), False)
