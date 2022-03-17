ceecaa298eecaeec17dd5248be4322343681f2f0
Nishant Kheterpal <nishantkheterpal@gmail.com>
2022-03-16 20:11:08 -0700

    two different install scripts, remote vs local

---
+++ install_pip_remote.sh
@@ -0,0 +1,10 @@
+pip install cycler
+pip install kiwisolver
+pip install matplotlib
+pip install mpmath
+pip install numpy
+pip install Pillow
+pip install pyparsing
+pip install python_dateutil
+pip install six
+pip install sympy
